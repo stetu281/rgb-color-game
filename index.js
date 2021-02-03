712039f2da0143f6c0be3f8e92459ca7b72e61ad
Stefan Turner <turner.stefan281@gmail.com>
2021-02-03 20:42:38 +0100

    Add gameplay function

--- index.js
+++ index.js
@@ -18,10 +18,26 @@ function fillCircles() {
 function selectRandomCircle(colorFields) {
     let randomNum = Math.floor(Math.random() * 6);
     let winnerColor = colorFields[randomNum].style.backgroundColor;
-    console.log(winnerColor)
+    document.querySelector('#rgb-color').innerHTML = winnerColor;
+    startGame(winnerColor, colorFields);
 };
 
+function startGame(winner, colorFields) {
 
+    for(let field of colorFields) {
+        field.addEventListener('click', function(e) {
+            
+            if(e.target.style.backgroundColor === winner) {
+                let overlay = document.querySelector('.win-overlay');
+                overlay.style.backgroundColor = winner;
+                overlay.classList.add('show-win-overlay');
+            } else {
+                e.target.classList.add('disable');
+            }
+
+        })
+    }
+};
 
 
 
