3b12e3994c05334d3e1a5d1af90d3e8804bcdbc7
Stefan Turner <turner.stefan281@gmail.com>
2021-02-03 20:11:29 +0100

    Add Circle fill function and select winning circle function

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ function generateColor() {
         arr.push(Math.floor(Math.random() * 256 ));
     }
     return 'rgb(' + arr.join() + ')';
-}
+};
 
-console.log(generateColor());
+function fillCircles() {
+    let colorFields = document.querySelectorAll('.color-field');
+
+    for(let colorField of colorFields) {
+        colorField.style.backgroundColor = generateColor();
+    }    
+    selectRandomCircle(colorFields);
+};
+
+function selectRandomCircle(colorFields) {
+    let randomNum = Math.floor(Math.random() * 6);
+    let winnerColor = colorFields[randomNum].style.backgroundColor;
+    console.log(winnerColor)
+};
+
+
+
+
+
+fillCircles();
