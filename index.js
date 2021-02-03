a00833b6abe625eeea039b6696b917ec33d84fdb
Stefan Turner <turner.stefan281@gmail.com>
2021-02-03 20:54:50 +0100

    game Working

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+let overlay = document.querySelector('.win-overlay');
+let buttons = document.querySelectorAll('.new-game');
+
+fillCircles();
+
+for(let button of buttons) {
+    button.addEventListener('click', function() {
+        overlay.classList.remove('show-win-overlay');
+        let circles = document.querySelectorAll('.color-field');
+        for(let circle of circles) {
+            circle.classList.remove('disable');
+        }
+        fillCircles();
+    })
+}
+
 function generateColor() {
     let arr = [];
     for(let i = 0; i < 3; i++) {
@@ -27,8 +43,7 @@ function startGame(winner, colorFields) {
     for(let field of colorFields) {
         field.addEventListener('click', function(e) {
             
-            if(e.target.style.backgroundColor === winner) {
-                let overlay = document.querySelector('.win-overlay');
+            if(e.target.style.backgroundColor === winner) {     
                 overlay.style.backgroundColor = winner;
                 overlay.classList.add('show-win-overlay');
             } else {
@@ -41,4 +56,3 @@ function startGame(winner, colorFields) {
 
 
 
-fillCircles();
